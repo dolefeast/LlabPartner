deaf59b151da6b2efb17e7d9cb3242c1b5eec397
dolefeast <elrois456yt@gmail.com>
2023-03-01 18:21:50 +0100

    different k shapes

--- code/remove_bao.py
+++ code/remove_bao.py
@@ -15,12 +15,14 @@ df, params = util_tools.many_files(files)      #all pk smooth as outputs from cl
 
 print('Creating Olin data files...')
 for data, omega in zip(df, params):
-    Om, OL, Ok = omega
+    Om, OL, Ok = (0.31,0.69,0.)
     k_in, pk_in = np.array(data[0]), np.array(data[1])
     pk_nobao = util_tools.remove_bao(k_in, pk_in)
     olin = util_tools.calculate_olin(k_in, pk_in)
     olin_df = pd.DataFrame([k_in.T, olin.T]).T
+    plt.plot(k_in, olin_df[0], '+')
     print(f'\tPrinting to Olin_Om0{int(100*Om)}_OL0{int(100*OL)}.txt...')
-    olin_df.to_csv(f'/home/santi/TFG/outputs_santi/class_outputs/Olin_Om0{int(100*Om)}_OL0{int(100*OL)}.txt', sep='\t', index=False)
+    olin_df.to_csv(f'/home/santi/TFG/outputs_santi/class_outputs/Olin_np_Om0{int(100*Om)}_OL0{int(100*OL)}.txt', sep='\t', index=False)
 
 print('Done!')
+plt.show()
